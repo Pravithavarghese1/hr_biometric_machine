f8202931d02c6111c1507c0886c36e47933f9400
Pravitha Varghese <pravithavarghese1@gmail.com>
2017-08-20 09:08:26 +0300

    Update __manifest__.py

--- hr_biometric_machine/__manifest__.py
+++ hr_biometric_machine/__manifest__.py
@@ -14,7 +14,7 @@ Downloads the logs of each employee from the fingerprint machine with their deta
     """,
 
     'author': "Pravitha V",
-    'website': "pravitha.wordpress.com",
+    'website': "https://www.odoo.com/",
     'category': 'Human Resources Custom',
     'version': '0.1',
 
